ff7a90a6ddacff3ffa0da8a6a2cefed13be99909
gaoyang3513 <gaoyang3513@outlook.com>
2023-07-09 20:43:19 +0800

    [修改] 增加 目录页 network
    [详细说明]
        1. 增加目录页：network
        2. 增加互斥锁，优化按键(死锁？)

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ pcf8574_out   = {'LED2':1,  'L3':0,    'L4':0,    'BUZZ':1}
 pcf8574_ports = {**pcf8574_in, **pcf8574_out}
 
 def pcf8574_callbak(port_val) :
+	global press_keys
 	#print('Port val: %X' % port_val)
 
 	i = 0
@@ -44,23 +45,19 @@ def pcf8574_callbak(port_val) :
 	time.sleep(0.2)
 	GPIO.output(GPIO_LED_RED, 0)
 
-def menu_main(press_keys):
+def menu_network(press_keys):
 	global menu_page
-
-	if menu_page != 0:
-		return
 
 	menu_item = 0
 	if 'KEY_L' in press_keys :
-		if menu_page :
-			menu_page = menu_page - 1
+		menu_page = (menu_page - 1) % len(menu_list)
+	if 'KEY_R' in press_keys :
+		menu_page = (menu_page + 1) % len(menu_list)
 	if 'KEY_U' in press_keys :
 		if menu_item :
 			menu_item = menu_item - 1
 	if 'KEY_D' in press_keys :
-		menu_item = (menu_item + 1) % len(menu_list)
-	if 'KEY_R' in press_keys :
-		menu_page = (menu_page + 1) % len(menu_list)
+		menu_item = menu_item + 1
 
 	try:
 		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -69,15 +66,32 @@ def menu_main(press_keys):
 	finally:
 		s.close()
 
+	oled.draw_rectangle(0, 16, 128, 64)
+	oled.draw_text(0, 16, 14, 'IP: %s' % ip)
+	oled.flush()
+
+def menu_main(press_keys):
+	global menu_page
+
+	menu_item = 0
+	if 'KEY_L' in press_keys :
+		menu_page = (menu_page - 1) % len(menu_list)
+	if 'KEY_R' in press_keys :
+		menu_page = (menu_page + 1) % len(menu_list)
+	if 'KEY_U' in press_keys :
+		if menu_item :
+			menu_item = menu_item - 1
+	if 'KEY_D' in press_keys :
+		menu_item = menu_item + 1
+
 	temperature = bmp180.read_temperature()
 	pressure    = bmp180.read_pressure()
 	oled.draw_rectangle(0, 16, 128, 64)
-	oled.draw_text(0, 16, 14, 'IP: %s' % ip)
-	oled.draw_text(0, 32, 14, u'温度: %.2f℃'  % temperature)
-	oled.draw_text(0, 48, 14, u'气压: %.2fkPa' % (pressure/1000))
+	oled.draw_text(0, 16, 14, u'温度: %.2f℃'  % temperature)
+	oled.draw_text(0, 32, 14, u'气压: %.2fkPa' % (pressure/1000))
 	oled.flush()
 
-menu_list = {'main':menu_main}
+menu_list = [['main', menu_main], ['network', menu_network]]
 bmp180 = BMP180.BMP180()
 oled = OLED.OLED(OLED_GPIO_RST, OLED_GPIO_DC, OLED_SPI_BUS, OLED_SPI_CS)
 menu_page = 0
@@ -94,11 +108,12 @@ def main():
 	oled.draw_text(0,  0, 14, 'Raspi-SmartHome')
 	oled.flush()
 
-	while True:
-		for page in menu_list:
-			menu_list[page](press_keys)
+	print('Menu list size: %u' % len(menu_list))
 
+	while True:
+		menu_list[menu_page][1](press_keys)
 		press_keys = []
+
 		time.sleep(1)
 
 if __name__=='__main__':
